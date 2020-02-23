cc9d2c435d6367f00fc2a793cfb708b4f4d86377
Survari <tristanpieper080803@gmail.com>
2020-02-23 15:53:24 +0100

    Moved tstring.hpp to extra direcory.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
-//
-// Created by trist on 13.01.2019.
-//
-
 #include <iostream>
-#include "tstring.hpp"
+#include <stdexcept>
+#include "include/tstring.hpp"
+
+#define TEST(a, b) if (a != b) { std::cout << "\033[1;31mTESTING-ERROR:\033[00m " << a << " != " << b << std::endl; throw std::runtime_error("test failed. aborted."); } else { std::cout << "\033[1;32mTEST-SUCCESS:\033[00m " << a << " == " << b << std::endl; }
+#define STR_TEST(a, b) TEST(a, std::string(b))
 
 int main()
 {
     tri::string s = 'H';
-
-    std::cout << s << std::endl;
+    STR_TEST(s, "H");
 
     s = s + "ello world";
-    s += '!';
+    STR_TEST(s, "Hello world");
 
-    std::cout << s << std::endl;
+    s += '!';
+    STR_TEST(s, "Hello world!");
 
     s = s.replace("Hello", "Good night,");
+    STR_TEST(s, "Good night, world!");
 
-    std::cout << s << std::endl;
-    std::cout << s.toUpper().split(',')[0]+"!" << std::endl;
+    s = s.toUpper().split(',')[0]+"!";
+    STR_TEST(s, "GOOD NIGHT!");
 
-    if (s.equalsIgnoreCase("good night, world!"))
-        std::cout << "ok" << std::endl;
-
-    std::cout << s << std::endl;
+    s = s.substr(0, s.size() - 2)+", world!";
+    TEST(s.equalsIgnoreCase("good night, world!"), true);
 
     if (s.endsWith("world!") && s.startsWith("Good"))
         std::cout << "Good world!" << std::endl;
+
+    s = s.substr(0, 1);
+    STR_TEST(s, "G");
 }
